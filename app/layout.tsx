814b5f5a712a4cbd9a9f9a0e0c7738719f6952f7
Ras JOJO <95353521+JoelaRAS@users.noreply.github.com>
2024-07-14 06:43:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,29 @@
-// layout.tsx
-import "./globals.css"
-import { Figtree } from "next/font/google"
+import "./globals.css";
+import { Figtree } from "next/font/google";
 
-import Sidebar from "../components/Sidebar"
-import SupabaseProvider from "@/providers/SupabaseProvider"
-import UserProvider from "@/providers/UserProvider"
-import ModalProvider from "@/providers/ModalProvider"
-import ToasterProvider from "@/providers/ToasterProvider"
-import getSongsByUserId from "@/actions/getSongsByUserId"
-import Player from "@/components/Player"
-import Header from "@/components/Header"
+import Sidebar from "../components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
+import { Web3Modal } from "@/providers/web3Provider";
 
-const figtree = Figtree({ subsets: ["latin"] })
+const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Whippin Music App",
-}
+};
 
-export const revalidate = 0
+export const revalidate = 0;
 
 interface LayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default async function RootLayout({ children }: LayoutProps) {
-  const userSongs = await getSongsByUserId()
+  const userSongs = await getSongsByUserId();
 
   return (
     <html lang="en">
@@ -33,14 +32,14 @@ export default async function RootLayout({ children }: LayoutProps) {
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider>
-              <Sidebar songs={userSongs}>
-                  {children}
-              </Sidebar>
+              <Web3Modal>
+                <Sidebar songs={userSongs}>{children}</Sidebar>
+              </Web3Modal>
               <Player />
             </ModalProvider>
           </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
-  )
+  );
 }
